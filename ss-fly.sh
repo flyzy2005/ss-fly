27719f19f9c3b2bd257d55250676c0035083db3d
Flyzy2005 <flyzy2005@gmail.com>
2018-01-31 12:42:03 +0800

    判断是否已经添加自启动 && 增加自定义端口，默认端口改为1024

--- ss-fly.sh
+++ ss-fly.sh
@@ -11,13 +11,24 @@ wrong_para_prompt() {
 }
 
 install() {
-	if [[ "$#" -ne 1 ]]; then
-		wrong_para_prompt "请输入密码"
-		return 1
+	if [[ "$#" -lt 1 ]]
+        then
+          wrong_para_prompt "请输入至少一个参数作为密码"
+	  return 1
 	fi
+        port="1024"
+        if [[ "$#" -ge 2 ]]
+        then
+          port=$2
+        fi
+        if [[ $port -le 0 || $port -gt 65535 ]]
+        then
+          wrong_para_prompt "端口号输入格式错误，请输入1到65535"
+          exit 1
+        fi
 	echo "{
     \"server\":\"0.0.0.0\",
-    \"server_port\":8388,
+    \"server_port\":$port,
     \"local_address\": \"127.0.0.1\",
     \"local_port\":1080,
     \"password\":\"$1\",
@@ -32,7 +43,11 @@ install() {
 	chmod 755 /etc/shadowsocks.json
 	apt-get install python-m2crypto
 	ssserver -c /etc/shadowsocks.json -d start
-	sed -i '$i\ssserver -c /etc/shadowsocks.json -d start'  /etc/rc.local
+	rclocal=`cat /etc/rc.local`
+        if [[ $rclocal != *'ssserver -c /etc/shadowsocks.json -d start'* ]]
+        then
+          sed -i '$i\ssserver -c /etc/shadowsocks.json -d start'  /etc/rc.local
+        fi
 	echo '安装成功~尽情冲浪吧'
 }
 
@@ -92,7 +107,7 @@ fi
 
 case $1 in
 	-i|i|install )
-        install $2
+        install $2 $3
 		;;
         -bbr )
         install_bbr
